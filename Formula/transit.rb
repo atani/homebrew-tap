5be1f65a55c2aca929fe8c792638b16993b35112
atani <atani@users.noreply.github.com>
2026-06-26 12:17:09 +0900

    Update transit to 0.3.0

--- Formula/transit.rb
+++ Formula/transit.rb
@@ -1,8 +1,8 @@
 class Transit < Formula
   desc "Japan transit routes and station departures CLI"
   homepage "https://github.com/atani/transit"
-  url "https://github.com/atani/transit/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "88a616959e003df3bc4f6418b053445f92635ba4f1597859b15536cf7aac87bf"
+  url "https://github.com/atani/transit/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "7ef9defed2aa4d38d29218a8621155c3a259a9afa68993abed7d0abedddeec3a"
   license "MIT"
   head "https://github.com/atani/transit.git", branch: "main"
 
@@ -13,6 +13,6 @@ class Transit < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/transit version")
+    assert_match version.to_s, shell_output("#{bin}/transit --version")
   end
 end
